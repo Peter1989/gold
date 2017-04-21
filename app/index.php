f18acd9e26f247281b75a69ed3aa334f19e52d8d
dongyang1989 <translate@yeah.net>
2017-04-22 06:35:32 +0800

    fix bug

--- app/index.php
+++ app/index.php
@@ -6,6 +6,9 @@ $date = date('Ymd');
 <head>
 <title>首页</title>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<style type="text/css">
+a {font-size: 200%}
+</style>
 </head>
 <body>
 <a href="/goldinfo/price/<?php echo $date?>">当日金价</a>
