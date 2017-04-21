c4f79628e109b175e5d03d2f3a13c6f3508ad9ba
dongyang1989 <translate@yeah.net>
2017-04-22 00:09:33 +0800

    首页内容

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<!Doctype Html>
+<html>
+<head>
+<title>首页</title>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+</head>
+<body>
+<a href="goldinfo/price">当日金价</a>
+</body>
