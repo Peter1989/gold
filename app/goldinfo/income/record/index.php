8f433866ed399364276194cb6270cf26b3ff0b89
dongyang1989 <translate@yeah.net>
2017-04-23 21:03:37 +0800

    添加收入记录页

--- app/goldinfo/income/record/index.php
+++ app/goldinfo/income/record/index.php
@@ -5,9 +5,39 @@ $res = require_once($inform_path);
 
 $record_data = new RecordData();
 $res = $record_data->get_income_records(1);
+$list = '<span>';
+$change = '';
+$sum = 0;
+foreach($res as $v){
+    if($v['increase'] == '1'){
+        $change = '收入了'; 
+        $sum += $v['amount'];
+    }else{
+        $change = '亏损了';
+        $sum -= $v['amount'];
+    }
 
+    $list .= $v['timestamp'].'&nbsp;&nbsp;'.$change.'&nbsp;&nbsp;'.$v['amount'].'元</span><br><span>'; 
+}
 
+$list_out = rtrim($list, '<span>'); 
 
 ?>
 
+<!Doctype Html>
+<html>
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<style>
+body{background-color:#bfd0c4;}
+</style>
+</head>
+<body>
+<br>
+<span>总收入：<?php echo $sum?></span>
+<br><br>
+<?php echo $list_out?>
 
+</body>
+</html>
--- app/index.php
+++ app/index.php
@@ -7,10 +7,12 @@ $date = date('Ymd');
 <title>首页</title>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <style type="text/css">
+body{background-color:#bfd0c4;}
 a {font-size: 200%}
 </style>
 </head>
 <body>
+<br>
 <a href="/goldinfo/price/date/<?php echo $date?>">当日金价</a><br>
 <a href="/goldinfo/income/record">盈亏记录</a><br>
 </body>
